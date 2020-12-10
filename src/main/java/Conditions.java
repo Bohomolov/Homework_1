07497121e662658b0253536fd1873d613784bc29
I-DEMPSE-I <73234562+I-DEMPSE-I@users.noreply.github.com>
2020-12-10 21:44:53 +0200

    ...

--- src/main/java/Conditions.java
+++ src/main/java/Conditions.java
@@ -1,8 +1,9 @@
 public class Conditions {
-
+    //#=====================================_1ST_===========================================
     public int getSumOrMultiply(int a, int b) {
         return (a % 2 == 0) ? a * b : a + b;
     }
+    //#=====================================_2ND_==========================================
     public String showQuarter(int x, int y) {
         String result;
         if (x > 0 && y > 0) {
@@ -26,6 +27,7 @@ public class Conditions {
         }
         return result;
     }
+    //#=====================================_3RD_============================================
     public int getSum(int a, int b, int c) {
         int result = 0;
         if (a > 0) {
@@ -39,7 +41,7 @@ public class Conditions {
         }
         return result;
     }
-
+    //#=====================================_4TH_==============================================
     public int countMax(int a, int b, int c) {
         int result;
         int sum = a + b + c;
@@ -51,22 +53,23 @@ public class Conditions {
         }
         return result;
     }
-    public String rating(int a) {
-        if (a < 0 || a > 100 ){
+    //#=====================================_5TH_===============================================
+    public String showRating(int argument) {
+        if (argument < 0 || argument > 100 ){
            throw new IllegalArgumentException("Incorrect value, correct value [0 - 100]");
         }
         String result ="";
-        if (a >= 0 && a <= 19) {
+        if (argument >= 0 && argument <= 19) {
             result = "F";
-        } else if ( a <= 39) {
+        } else if ( argument <= 39) {
             result = "E";
-        } else if ( a <= 59) {
+        } else if ( argument <= 59) {
             result = "D";
-        } else if ( a <= 74) {
+        } else if ( argument <= 74) {
             result = "C";
-        } else if ( a <= 89) {
+        } else if ( argument <= 89) {
             result = "B";
-        } else if ( a <= 100) {
+        } else if ( argument <= 100) {
             result = "A";
         }
         return result;
--- src/main/java/Loops.java
+++ src/main/java/Loops.java
@@ -1,70 +1,80 @@
 public class Loops {
-
+    //#=====================================_1ST_===========================================
     public String countSumEvenNumber() {
-        String result = "";
         int sum = 0;
         int quantity = 0;
         for (int i = 2; i < 100; i += 2) {
             sum += i;
-            quantity += 1 ;
+            quantity += 1;
         }
-        result += "Sum = " + sum + " Quantity = " + quantity;
-        return result;
+       return  "Sum = " + sum + " Quantity = " + quantity;
     }
-
-    String resultPrime;
-
-    public String showPrime(int a) {
+    //#=====================================_2ND_==========================================
+    public String checkIsNumberPrime(int argument) {
+        String resultPrime = "";
+        if (argument <= 0) {
+            throw new IllegalArgumentException("Incorrect value! The number should be equals 2 or more.");
+        }
         int i = 2;
-        while (i <= a) {
-            if (a % i != 0) {
+        while (i <= argument) {
+            if (argument % i != 0) {
                 i++;
-            } else if (a % i == 0) {
-                outL(a, i);
+            } else if (argument % i == 0) {
+                if (argument == i) {
+                    resultPrime = "This number is prime";
+                } else {
+                    resultPrime = "This number isn't prime";
+                }
                 break;
             }
         }
         return resultPrime;
     }
-
-    public void outL(int a, int i) {
-        if (a == i) {
-            resultPrime = "This number is prime";
-        } else resultPrime = "This number isn't prime";
-    }
-
-    public int sqrt(int a) {
+    //#=====================================_3RT_==========================================
+    public int findSqrt(int argument) {
+        if (argument <= 0) {
+            throw new IllegalArgumentException("Incorrect value! The number should be equals 1 or more.");
+        }
         int resultSqrt = 0;
-        for (int i = 1; i <= a; i++) {
-            if (i * i == a) {
+        for (int i = 1; i <= argument; i++) {
+            if (i * i == argument) {
                 resultSqrt = i;
             }
         }
         return resultSqrt;
     }
-
-    public int factorial(int a) {
+    //#=====================================_4TH_==========================================
+    public int countFactorial(int argument) {
+        if (argument <= 0) {
+            throw new IllegalArgumentException("Incorrect value! The number should be equals 1 or more.");
+        }
         int result = 1;
-        for (int i = 1; i <= a; i++) {
+        for (int i = 1; i <= argument; i++) {
             result *= i;
         }
         return result;
     }
-
-    public int countSum(int e) {
+    //#=====================================_5TH_==========================================
+    public int countSumOfDigits(int argument) {
+        if (argument < 0) {
+            throw new IllegalArgumentException("Incorrect value! The number should be equals 0 or more.");
+        }
         int result = 0;
-        while (e > 0) {
-            result += e % 10;
-            e /= 10;
+        while (argument > 0) {
+            result += argument % 10;
+            argument /= 10;
         }
         return result;
     }
-
-    public String reverse(int e) {
+    //#=====================================_6TH_==========================================
+    public String reverseNumeral(int argument) {
+        if (argument <= 0) {
+            throw new IllegalArgumentException("Incorrect value! The number should be equals 1 or more.");
+        }
         String result = "";
-        while (e > 0) {
-            result += e % 10;
-            e /= 10;
+        while (argument > 0) {
+            result += argument % 10;
+            argument /= 10;
         }
         return result;
     }
