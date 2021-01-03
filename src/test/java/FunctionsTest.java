d47ea36ed8dd38d81faf6d62ebcffd54479c323a
Bohomolov <bogomolov9567@gmail.com>
2021-01-03 20:30:50 +0200

    Update

--- src/test/java/FunctionsTest.java
+++ src/test/java/FunctionsTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import utils.Constants;
 
 import static org.junit.Assert.assertEquals;
 
@@ -34,6 +35,7 @@ public class FunctionsTest {
             functions.convertNumberToString(45 - 789);
         }, "Incorrect value! The number should be equals 1 or more.");
     }
+
     @Test
     public void testShowDayOfTheWeek_exception_0() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
@@ -175,6 +177,7 @@ public class FunctionsTest {
         String actual = functions.convertNumberToString(500);
         assertEquals(expected, actual);
     }
+
     @Test
     public void convertToString_719() {
         String expected = "Seven hundred Nineteen ";
@@ -209,24 +212,125 @@ public class FunctionsTest {
             functions.convertNumberToString(45 - 789);
         }, "Incorrect value! The number should be equals 1 or more.");
     }
+
     @Test
     public void testReverseNumeral_exception_0() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             functions.convertNumberToString(0);
         }, "Incorrect value! The number should be equals 1 or more.");
     }
-    //============================_2nd_method_end_==================================================
+
+    //============================_2nd_method_end_=================================================
     //============================_3rd_method_start_===============================================
+
     @Test
-    public void convertStringToNumber_1() {
-        String expected = "1";
-        String actual = functions.convertStringToNumber("One ");
+    public void convertToInt_1() {
+        int expected = 1;
+        int actual = functions.convertStringToNumber("one");
         assertEquals(expected, actual);
     }
     @Test
-    public void convertStringToNumber_17() {
-        String expected = "17";
-        String actual = functions.convertStringToNumber("Seventeen ");
+    public void convertToInt_2() {
+        int expected = 2;
+        int actual = functions.convertStringToNumber("two");
         assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_5() {
+        int expected = 5;
+        int actual = functions.convertStringToNumber("five");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_15() {
+        int expected = 15;
+        int actual = functions.convertStringToNumber("fifteen");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_25() {
+        int expected = 25;
+        int actual = functions.convertStringToNumber("twenty five");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_50() {
+        int expected = 50;
+        int actual = functions.convertStringToNumber("fifty");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_57() {
+        int expected = 57;
+        int actual = functions.convertStringToNumber("fifty seven");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_100() {
+        int expected = 100;
+        int actual = functions.convertStringToNumber("one hundred");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_101() {
+        int expected = 101;
+        int actual = functions.convertStringToNumber("one hundred one");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertToInt_105() {
+        int expected = 105;
+        int actual = functions.convertStringToNumber("one hundred five");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_110() {
+        int expected = 110;
+        int actual = functions.convertStringToNumber("one hundred ten");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_120() {
+        int expected = 120;
+        int actual = functions.convertStringToNumber("one hundred twenty");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_150() {
+        int expected = 150;
+        int actual = functions.convertStringToNumber("one hundred fifty");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_155() {
+        int expected = 155;
+        int actual = functions.convertStringToNumber("one hundred fifty five");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_283() {
+        int expected = 283;
+        int actual = functions.convertStringToNumber("two hundred eighty three");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_300() {
+        int expected = 300;
+        int actual = functions.convertStringToNumber("three hundred");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void convertToInt_999() {
+        int expected = 999;
+        int actual = functions.convertStringToNumber("nine hundred ninety nine");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convertToInt_exception_0() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            functions.convertStringToNumber("sssss");
+        }, Constants.INCORRECT_VALUE_S);
     }
 }
